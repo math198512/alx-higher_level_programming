fb22a1fe85897e0c9fef36a5d744f497116ef30e
math198512 <thoraichi@gmail.com>
2023-11-02 03:00:53 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Do some Maths, and prints the result"""
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
 
     a = 10
     b = 5

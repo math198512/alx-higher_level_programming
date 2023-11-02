4c91b2a34791b743661bf90c290c11e6cba78fe6
math198512 <thoraichi@gmail.com>
2023-11-02 10:53:44 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+n = len(sys.argv) - 1
+if n == 0:
+    print("{} arguments.".format(n))
+else:
+    print("{} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]))

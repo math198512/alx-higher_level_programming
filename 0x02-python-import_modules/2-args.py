e098e553224b0b821b7cae00ff6dfdeb8b34a966
math198512 <thoraichi@gmail.com>
2023-11-02 10:58:29 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import sys
-n = len(sys.argv) - 1
-if n == 0:
-    print("{} arguments.".format(n))
-else:
-    print("{} arguments:".format(n))
-    for i in range(1, n + 1):
+if __name__ == "__main__":
+    """Print the number of and list of arguments"""
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("{} arguments.".format(n))
+    else:
+        print("{} arguments:".format(n))
+        for i in range(1, n + 1):
         print("{}: {}".format(i, sys.argv[i]))

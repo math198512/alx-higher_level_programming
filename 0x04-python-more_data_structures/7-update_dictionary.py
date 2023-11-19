51ed562c0738fd1f699023ce6e6e861e2dbbd669
math198512 <thoraichi@gmail.com>
2023-11-19 13:25:36 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for k in sorted(a_dictionary):
-        if k == key:
-            a_dictionary[k] = value
-            return (a_dictionary)
+    a_dictionary[key] = value
+    return (a_dictionary)

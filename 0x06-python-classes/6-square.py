8fb4f5b08487cf451fc01212a1127ad6769ea162
math198512 <thoraichi@gmail.com>
2023-11-21 23:41:00 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""class Square that defines a square"""
+"""Square class to represent a square"""
 
 
 class Square():
     """square class with it's size and proper validation"""
 
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        """"Initialize data"""
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
+        """"get size"""
         return self.__size
 
     @property
     def position(self):
+        """"get position"""
         return self.__position
 
     @size.setter
     def size(self, value):
+        """"set size"""
         if (type(value) is not int):
             raise TypeError("size must be an integer")
         elif (value < 0):
             raise ValueError("size must be >= 0")
-        self.__size = value
+        else:
+            self.__size = value
 
     @position.setter
     def position(self, value):
@@ -40,18 +45,16 @@ class Square():
             self.__position = value
 
     def area(self):
-
-        return (self.__size * self.__size)
+        """"get area of the square"""
+        return self.size ** 2
 
     def my_print(self):
-        size = self.__size
-        if size == 0:
+        """print the square"""
+        if self.size == 0:
             print()
         else:
-            for i in range(self.__position[1]):
+            for i in range(self.position[1]):
                 print("")
-            for i in range(size):
-                print(" " * self.__position[0], end="")
-                for j in range(size):
-                    print("#", end="")
-                print()
+            for i in range(self.size):
+                print(" " * self.position[0], end="")
+                print("#" * self.size)

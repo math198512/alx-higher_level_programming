b2fd0459a979c70a95694bbc6f1579302fb89a91
math198512 <thoraichi@gmail.com>
2023-11-21 23:42:52 -0800

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,6 +53,8 @@ class Square():
         if self.size == 0:
             print()
         else:
+            for i in range(self.position[1]):
+                print("")
             for i in range(self.size):
                 print(" " * self.position[0], end="")
                 print("#" * self.size)

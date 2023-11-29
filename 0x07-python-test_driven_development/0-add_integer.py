9a9bf407bb38de4e3862a8f4161a23674819012d
math198512 <thoraichi@gmail.com>
2023-11-29 01:43:46 -0800

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,9 +4,22 @@ This function, add_integer(),
 adds together 2 int or float types and returns an int.
 """
 
+
 def add_integer(a, b=98):
+
     """Return the sum of two integers or floats as an integer.
     Otherwise raise a TypeError for given incorrect argument type.
+    Usage:
+    >>> add_integer(15, 0)
+    15
+    >>> add_integer(0, 15)
+    15
+    >>> add_integer(-2, 2)
+    0
+    >>> add_integer(2, -2)
+    0
+    >>> add_integer(2)
+    100
     """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")

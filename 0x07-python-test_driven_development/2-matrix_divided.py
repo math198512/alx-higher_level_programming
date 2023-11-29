e973d14aee14c51fefc6fc50fc8ae5cd93272654
math198512 <thoraichi@gmail.com>
2023-11-29 07:20:51 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -9,20 +9,20 @@ def matrix_divided(matrix, div):
         forms a new matrix after all elements are divided
     """
 
-    new_matrix = []
-    if type(div) not in [int, float]:
+    if (not isinstance(matrix, list) or matrix == [] or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, int) or isinstance(ele, float))
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    for row in matrix:
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        transpose = []
-        for elem in row:
-            if type(elem) not in [int, float]:
-                raise TypeError("matrix must be a matrix (list of lists)\
- of integers/floats")
-            elem = round(elem / div, 2)
-            transpose.append(elem)
-        new_matrix.append(transpose)
-    return new_matrix
+
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])

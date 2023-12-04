59fa119146aae2aa60efe6578c1c39c2d493cbce
math198512 <thoraichi@gmail.com>
2023-12-04 10:44:35 -0800

    10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Module for Square class.'''
+Rectangle= __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square (subclass of Rectangle) definition"""
+    def __init__(self, size):
+        '''Constructor.'''
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+
+        """compuctes are of a square"""
+        return (self.__size * self.__size)

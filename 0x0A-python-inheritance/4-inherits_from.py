b1e274ead3a9a879a7b39a3e0cf666983e5f5931
math198512 <thoraichi@gmail.com>
2023-12-04 03:35:20 -0800

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Defines a function to check the subclasses of an object."""
+
+def inherits_from(obj, a_class):
+    """Check if an object is an instance of the subclasses of a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj is an instance of the subclasses of a_class - True.
+        Otherwise - False.
+    """
+    
+    if (type(obj) != a_class):
+        return (issubclass(type(obj), a_class))
+    return (False)

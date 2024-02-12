ab157ec6900ee2884c0a72d8e170b7a45837acee
math198512 <thoraichi@gmail.com>
2024-02-12 13:43:58 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,4 +3,3 @@
 const { argv } = require('node:process');
 
 if (argv.length === 2) { console.log('No argument'); } else if (argv.length === 3) { console.log('Argument found'); } else { console.log('Arguments found'); }
-console.log(message);

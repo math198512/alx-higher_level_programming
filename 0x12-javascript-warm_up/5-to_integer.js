56187e67cb20451e595355b34c49718ae388f1be
math198512 <thoraichi@gmail.com>
2024-02-15 09:42:58 -0800

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,7 @@
 const { argv } = require('process');
 const argv1 = parseInt(argv[2]);
 if (isNaN(argv1)) {
-	console.log('Not a number')
-}
-else {
-	console.log('My number: ' + argv1);
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + argv1);
 }

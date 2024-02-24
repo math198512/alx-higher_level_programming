8353287a3292459b83063ca1534652448ad0608f
math198512 <thoraichi@gmail.com>
2024-02-24 01:40:51 -0800

    model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,7 +4,6 @@ Defines a state model that contain the class definition
  of a State and an instance Base = declarative_base()
 """
 
-from lib2to3.pytree import Base
 from sre_parse import State
 from unicodedata import name
 from sqlalchemy import Column, Integer, String

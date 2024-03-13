74518567387be254719ccef03a0475f1f117cec4
math198512 <thoraichi@gmail.com>
2024-03-13 15:06:45 -0700

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,6 +98,7 @@ class Rectangle(Base):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+    
     def to_dictionary(self):
         """ method that returs a dictionary with properties """
         attrs = ['id', 'width', 'height', 'x', 'y']

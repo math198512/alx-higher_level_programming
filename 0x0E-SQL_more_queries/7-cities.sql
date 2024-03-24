92d03147cb2ae251cd159f7fa9a23c93cd192f2f
math198512 <thoraichi@gmail.com>
2024-03-24 11:24:22 -0700

    7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRAIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id)
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
 );

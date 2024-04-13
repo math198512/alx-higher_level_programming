6989f08b27ae311df939c15a1abf7dd91123974c
math198512 <thoraichi@gmail.com>
2024-04-13 02:35:17 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
-  console.log('code: ', response.statusCode); // Print the status code
+request(url, function (error, response) {
   console.error('error:', error); // Print the error if one occurred
+  console.log('code: ', response.statusCode); // Print the status code
 });
